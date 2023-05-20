3be2a97537fa95577f1e717185d266ce7f72c304
Pranav kumar <sanupranav@outlook.com>
2023-05-20 11:41:34 +0530

    Added speakers and schedule section

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./schedule.component.scss']
 })
 export class ScheduleComponent {
+  sessions: Session[] = [
+    new Session(1, 'Keynote', '09:00', 'Loveleen kaur','loveleen.jpg',
+    'Software Engineer - Astrotalk'),
+    
+    new Session(2, 
+      'Accelerate your key learnings of modern Android app development with KMM', 
+      '10:00', 'Adit Lal','Adit Lal.png',
+       'Google Developer Expert - Android'),
 
+    new Session(3, 
+      'Unleashing creativity with DCGANs', 
+      '11:00', 
+      'Nitin Tiwari',
+      'Nitin Tiwari.jpg', 
+      'Google Developer Expert - Machine Learning'),
+
+    new Session(4, 
+      'Scaling your Web-Apps using Firebase & Google Cloud', 
+      '12:00', 'Kartik Derasari',
+      'Kartik Derasari.png', 
+      'Google Developer Expert - Google Cloud'),
+
+    new Session(5, 
+      'Keeping Up with the Latest Trends in Web Development and Angular', 
+      '01:00', 
+      'Pranav Kumar Verma', 
+      'Pranav Kumar Verma.png',
+     'Senior Engineer - Nagarro'),
+  ];
+}
+export class Session {
+  id: number;
+  title: string;
+  startTime: string;
+  speakerName: string;
+  designation: string;
+  imageName: string;
+
+  constructor(id: number, title: string, startTime: string, speakerName: string, imageName: string, designation: string) {
+    this.id = id;
+    this.title = title;
+    this.startTime = startTime;
+    this.speakerName = speakerName;
+    this.imageName = imageName;
+    this.designation = designation;
+  }
 }
--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SpeakerDialogComponent } from '../speaker-dialog/speaker-dialog.component';
 
@@ -7,51 +7,107 @@ import { SpeakerDialogComponent } from '../speaker-dialog/speaker-dialog.compone
   templateUrl: './speakers.component.html',
   styleUrls: ['./speakers.component.scss']
 })
-export class SpeakersComponent {
-  speakers = [
-    {
-      name: 'Loveleen Kaur',
-      company: 'Astrotalk',
-      image: 'loveleen.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ultrices mauris, vitae ullamcorper sapien.'
-    },
-    {
-      name: 'Yatin Nayyar',
-      company: 'WIPRO',
-      image: 'yatin.jpeg',
-      description: 'Sed id elit eget elit vestibulum congue. Nullam vel orci vitae lacus laoreet hendrerit. '
-    },
-    {
-      name: 'Harneet Singh',
-      company: 'KPMG',
-      image: 'harneet.jpeg',
-      description: 'Phasellus in dui vel est eleifend mattis non ut nulla. Morbi vel sagittis enim.'
-    },
-    {
-      name: 'Loveleen Kaur',
-      company: 'Astrotalk',
-      image: 'loveleen.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel ultrices mauris, vitae ullamcorper sapien.'
-    },
-    {
-      name: 'Yatin Nayyar',
-      company: 'WIPRO',
-      image: 'yatin.jpeg',
-      description: 'Sed id elit eget elit vestibulum congue. Nullam vel orci vitae lacus laoreet hendrerit. '
-    },
-    {
-      name: 'Harneet Singh',
-      company: 'KPMG',
-      image: 'harneet.jpeg',
-      description: 'Phasellus in dui vel est eleifend mattis non ut nulla. Morbi vel sagittis enim.'
-    }
+export class SpeakersComponent implements OnInit {
+  speakers: Speaker[] = [
+    new Speaker(1, 'Keynote', '09:00', 'Loveleen kaur','loveleen.jpg',
+    `Loveleen Kaur, a bundle of positivity and happiness, 
+    this tech-savvy girl loves mountains and works wonders with technology. 
+    She is currently working as an Android Developer and result oriented professional with 5 years of experience. 
+    She is Core Team Member of of GDG Chandigarh and Google Android Educator. Hailing from the technology field, 
+    she loves logic, but also has a passion for creativity. In her free time, She loves to mentor people to help them 
+    learn and grow in their career.`, 'Software Engineer - Astrotalk', 'Astrotalk'),
+    
+    new Speaker(2, 
+      'Accelerate your key learnings of modern Android app development with KMM', 
+      '10:00', 'Adit Lal','Adit Lal.png',
+      `
+    Adit is a Senior Android Engineer, currently working as an Individual Consultant. 
+    He has been working in the industry for close to about 10+ years primarily helping build android mobile products and 
+      scaling them up. Some of his hobbies are Stargazing, Travel, and Landscape Photography.`,
+       'Google Developer Expert - Android', 'Google Developer Expert'),
+
+    new Speaker(3, 
+      'Unleashing creativity with DCGANs', 
+      '11:00', 
+      'Nitin Tiwari',
+      'Nitin Tiwari.jpg', 
+    `
+    Nitin is a Google Developer Expert in Machine Learning and an avid learner of ML who loves to understand how tech
+    works and share his knowledge and expertise with the community.`, 'Google Developer Expert - Machine Learning', 'Google Developer Expert'),
+
+    new Speaker(4, 
+      'Scaling your Web-Apps using Firebase & Google Cloud', 
+      '12:00', 'Kartik Derasari',
+      'Kartik Derasari.png', 
+    `
+    Kartik is a Technical Consultant by profession & an advocate for technology by passion. 
+    He has been recognized as a Google Developer Expert for Google Cloud. Being a 6X Google Cloud Certified Professional, 
+    he contributes to App-Dev & Analytics projects as a Full-Stack Engineer. 
+    He runs the Google Developers Group for Cloud professionals and he's been also recognized as a Google Cloud Champion 
+    Innovator for Serverless App Development. He has delivered over 50 technical Speakers and has hosted more than 
+    65 developer-focused events & conferences. He has also been a Google DSC Lead and an ML Facilitator of Google 
+    AI's Explore ML Program.`, 'Google Developer Expert - Google Cloud', 'Google Developer Expert'),
+
+    new Speaker(5, 
+      'Keeping Up with the Latest Trends in Web Development and Angular', 
+      '01:00', 
+      'Pranav Kumar Verma', 
+      'Pranav Kumar Verma.png',
+    `
+    Pranav is a Full Stack Developer working as a Senior Engineer at Nagarro. 
+    He has over 6.5 years of experience in the IT industry, and really passionate about what he do. His focus is on Angular, 
+    and he love working on both front-end and back-end of web applications.`, 'Senior Engineer - Nagarro', 'Nagarro'),
   ];
 
   constructor(@Inject(MatDialog) public dialog: MatDialog) {}
 
+  ngOnInit(): void {
+    this.sortSpeakersByName()
+  }
+
   openSpeakerDialog(speaker: any) {
     const dialogRef = this.dialog.open(SpeakerDialogComponent, {
-      data: { speaker }
+      data: { speaker },
+      width: '700px',
+      enterAnimationDuration: '800'
     });
+  }
+
+  sortSpeakersByName(): void {
+    this.speakers.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+
+}
+
+export class Speaker {
+  id: number;
+  title: string;
+  startTime: string;
+  name: string;
+  designation: string;
+  imageName: string;
+  bio: string;
+  company: string;
+
+  constructor(id: number, title: string, startTime: string, name: string, imageName: string, bio: string, designation: string,company: string) {
+    this.id = id;
+    this.title = title;
+    this.startTime = startTime;
+    this.name = name;
+    this.imageName = imageName;
+    this.bio = bio;
+    this.designation = designation;
+    this.company = company;
   }
 }
